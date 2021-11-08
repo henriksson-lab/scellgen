eb5a7873c76b2bdcc686df1db6aac66d518fbeb9
Debojyoti <debojyoti.das@umu.se>
2021-11-08 11:41:42 +0100

    typing.Literal

--- decoders.py
+++ decoders.py
@@ -1,4 +1,9 @@
-from typing import List, Optional, Literal
+from typing import List, Optional
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+
 
 import torch
 import torch.nn as nn
